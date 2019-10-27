dd881858568e75b8daf954ef336bf474107bd72a
jakub_banasiak <soccerkid.bany@gmail.com>
2019-10-27 08:59:11 +0100

    added all functions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import * as ELEMENTS from './elements.js';
 import { Http } from './http.js'
+import { WeatherData, WEATHER_PROXY_HANDLER } from './weather-data.js'
 
 const APP_ID = '1189a029ce3a35c1fbf29e84a2e443fc';
 
@@ -14,9 +15,20 @@ function searchWeather() {
     const URL = 'https://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=' + APP_ID;
     Http.fetechData(URL)
         .then(function(responseData) {
-            console.log(responseData);
+            const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0].description.toUpperCase());
+            const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
+            WEATHER_PROXY.temperature = responseData.main.temp;
+            updateWeather(WEATHER_PROXY);
         })
         .catch(function(error) {
             alert(error);
         })
+}
+
+function updateWeather(weatherData) {
+    ELEMENTS.ELEMENT_WEATHER_CITY.textContent = weatherData.cityName;
+    ELEMENTS.ELEMENT_WEATHER_DESCRIPTION.textContent = weatherData.description;
+    ELEMENTS.ELEMENT_WEATHER_TEMPERATURE.textContent = weatherData.temperature;
+
+    ELEMENTS.ELEMENT_WEATHER_BOX.getElementsByClassName.display = 'block';
 }
