8b3058ca3b958dbede2ac036098f680cf79481f8
jakub_banasiak <soccerkid.bany@gmail.com>
2019-10-27 08:22:11 +0100

    add http request

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 import * as ELEMENTS from './elements.js';
+import { Http } from './http.js'
+
+const APP_ID = '1189a029ce3a35c1fbf29e84a2e443fc';
 
 ELEMENTS.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    alert('click');
+    const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.value.trim();
+    if (CITY_NAME.length === 0) {
+        alert('Please enter a city name');
+    }
+
+    const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=' + APP_ID;
+    Http.fetechData(URL)
+        .then(function(responseData) {
+            console.log(responseData);
+        })
+        .catch(function(error) {
+            alert(error);
+        })
 }
